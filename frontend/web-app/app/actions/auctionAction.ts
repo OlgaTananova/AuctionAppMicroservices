ae2acd1308be359a80e24c4fb131572ac093a271
Olga Tananova <olga.tananova@gmail.com>
2024-07-23 16:42:52 -0700

    Added zustand state store

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -1,9 +1,9 @@
 'use server'
 import { Auction, PagedResult } from "@/types";
 
-export async function getData(pageNumber: number = 1): Promise<PagedResult<Auction>>{
+export async function getData(query: string): Promise<PagedResult<Auction>>{
 
-    const res = await fetch(`http://localhost:6001/search?PageSize=4&PageNumber=${pageNumber}`);
+    const res = await fetch(`http://localhost:6001/search?${query}`);
     if(!res.ok) throw new Error('Failed to fetch data');
     return res.json();
 }
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,37 +1,50 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
-import {Auction} from '../../types/index'
+import {Auction, PagedResult} from '../../types/index'
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionAction';
-
+import Filters from './Filters';
+import { useParams } from 'next/navigation';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import {shallow} from 'zustand/shallow';
+import qs from 'query-string';
 
 
 export default function Listings() {
 
-  const [auctions, setAuction] = useState<Auction[]>([]);
-  const [pageCount, setPageCount] = useState(0);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(state => ({
+    pageNumber: state.pageNumber,
+    pageSize: state.pageSize,
+    searchTerm: state.searchTerm
+  }), shallow);
+
+  const setParams = useParamsStore(state => state.setParams);
+  const url = qs.stringify({url: '', query: params})
+
+  function setPageNumber(pageNumber: number){
+    setParams({pageNumber})
+  }
 
   useEffect(()=>{
-    getData(pageNumber).then(data => {
-      console.log(data);
-      setAuction(data.result);
-      setPageCount(data.pageCount);
+    getData(url).then(data => {
+      setData(data)
     })
-  }, [pageNumber]);
+  }, [url]);
 
-  if (auctions.length === 0) return <h3>Loading...</h3>
+  if (!data) return <h3>Loading...</h3>
 
       return (
         <>
+        <Filters></Filters>
           <div className='grid grid-cols-4 gap-6'>
-              {auctions.map((auction)=> (
+              {data.result.map((auction)=> (
             <AuctionCard auction={auction} key={auction.id}/>
               ))}
           </div>
           <div className='flex justify-center mt-4'>
-            <AppPagination currentPage={pageNumber} pageCount={pageCount} pageChanged={setPageNumber}/>
+            <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
           </div>
     </>
   )
