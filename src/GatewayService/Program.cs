94fcc02f657a22041b1f4ad7c13451eca4311022
Olga Tananova <olga.tananova@gmail.com>
2024-08-06 17:30:14 -0700

    Added NotificationService

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -20,8 +20,19 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         // Set the claim type for the username
         options.TokenValidationParameters.NameClaimType = "username";
     });
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("customPolicy", b => {
+        b.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 app.MapReverseProxy();
 app.UseAuthentication();
 app.UseAuthorization();
---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,30 @@
+using MassTransit;
+using NotificationService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+ 
+    // Set the endpoint name formatter to use kebab case
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    // Configure RabbitMQ with host and credentials from configuration
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
